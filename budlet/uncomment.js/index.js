d08a96d2d4d5f12b44c5208fa36e9866f11843e4
ibtisam midlet <storebitid@gmail.com>
2023-06-02 09:54:16 +0100

    *

--- budlet/uncomment.js/index.js
+++ budlet/uncomment.js/index.js
@@ -1,13 +1,34 @@
 var fs = require('fs');
 var uncomment = require('./uncomment.js');
+var path = require('path');
 
-var str = fs.readFileSync(path.join(path.dirname('data.json'), '..'), 'utf8');
-console.log(str)
-/*
+var jsonFilePath = path.join(__dirname, '..', 'data.json'); // Build the correct file path
+var str = fs.readFileSync(jsonFilePath, 'utf8'); // Read the content of the JSON file
+
 var res = uncomment(str, {
     removeEmptyLines: false
 });
 
-fs.writeFileSync('budlet/data.uncom.json', res, 'utf8');
- 
+fs.writeFileSync('../data.uncom.json', res, 'utf8'); // Write the resulting data to a file
+
+
+/* following my standards ended in closed route
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+import fs from 'fs';
+import path from 'path';
+
+const jsonFilePath = path.join(path.dirname(import.meta.url), '..', 'data.json'); // Build the correct file path
+
+const uncommentModule = await import('./uncomment.js'); // Dynamically import the uncomment module
+const uncomment = uncommentModule.default || uncommentModule; // Retrieve the uncomment function from the module
+
+const json = require(jsonFilePath); // Use require to import the JSON data
+
+const res = uncomment(json, {
+    removeEmptyLines: false
+});
+
+fs.writeFileSync('budlet/data.uncom.json', JSON.stringify(res), 'utf8'); // Write the resulting data to a file
 */
