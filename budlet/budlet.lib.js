53040fe9de53d3536d6f1f62874a26ca94688eaa
ibtisam midlet <storebitid@gmail.com>
2024-02-22 14:47:06 +0100

    0

--- budlet/budlet.lib.js
+++ budlet/budlet.lib.js
@@ -2,7 +2,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 const jsonfile = require('jsonfile')
-const file = './data.json'
+const file = './data.uncom.json'
 jsonfile.readFile(file, function (err, obj) {
   if (err) console.error(err)
   console.dir(obj)
